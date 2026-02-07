13c755619d460fc19b93d3cf59a57ee16f6f05bc
nav-commits <53131408+nav-commits@users.noreply.github.com>
2026-02-07 16:51:32 -0500

    fixed car path

--- data/browseCarData.ts
+++ data/browseCarData.ts
@@ -1,14 +1,14 @@
 import { BrowseCarType } from "@/types/browseCarType";
   
   export const carTypes: BrowseCarType[] = [
-    { icon: "/icons/suv.svg", title: "SUV" },
-    { icon: "/icons/sedan.svg", title: "Sedan" },
-    { icon: "/icons/hatchback.svg", title: "Hatchback" },
-    { icon: "/icons/coupe.svg", title: "Coupe" },
-    { icon: "/icons/hybrid.svg", title: "Hybrid" },
-    { icon: "/icons/convertible.svg", title: "Convertible" },
-    { icon: "/icons/van.svg", title: "Van" },
-    { icon: "/icons/truck.svg", title: "Truck" },
-    { icon: "/icons/electric.svg", title: "Electric" },
+    { icon: "/Icons/suv.svg", title: "SUV" },
+    { icon: "/Icons/sedan.svg", title: "Sedan" },
+    { icon: "/Icons/hatchback.svg", title: "Hatchback" },
+    { icon: "/Icons/coupe.svg", title: "Coupe" },
+    { icon: "/Icons/hybrid.svg", title: "Hybrid" },
+    { icon: "/Icons/convertible.svg", title: "Convertible" },
+    { icon: "/Icons/van.svg", title: "Van" },
+    { icon: "/Icons/truck.svg", title: "Truck" },
+    { icon: "/Icons/electric.svg", title: "Electric" },
   ];
   
